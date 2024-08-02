12ac6c3980b51704dec8ae649abaf92a5e36caef
Hunerk <41666504+Hunerk@users.noreply.github.com>
2024-08-02 11:17:56 +0800

    Update check_ct8.py

--- check_ct8.py
+++ check_ct8.py
@@ -12,16 +12,6 @@ def send_telegram_message(message):
     payload = {
         'chat_id': TELEGRAM_CHAT_ID,
         'text': message,
-        'reply_markup': {
-            'inline_keyboard': [
-                [
-                    {
-                        'text': '问题反馈❓',
-                        'url': 'https://t.me/yxjsjl'
-                    }
-                ]
-            ]
-        }
     }
     headers = {
         'Content-Type': 'application/json'
